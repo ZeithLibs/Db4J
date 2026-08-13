3f3e5b37119224be140dd0e0680b0fba1a29a5d8
Zeith <me@zeith.org>
2026-08-13 03:00:23 +0300

    2.0.3: UUIDv7 generation at a given timestamp.

--- src/main/java/dev/zeith/db4j/util/UUIDv7.java
+++ src/main/java/dev/zeith/db4j/util/UUIDv7.java
@@ -9,10 +9,14 @@ public class UUIDv7
 	
 	public static UUID randomUUID()
 	{
-		long timestamp = System.currentTimeMillis();
-		var rng = ThreadLocalRandom.current();
-		long msb = (timestamp << 16) | 0x7000L | (rng.nextLong() & 0x0FFFL);
-		long lsb = (rng.nextLong() & 0x3FFF_FFFF_FFFF_FFFFL) | 0x8000_0000_0000_0000L;
+		return randomUUID(System.currentTimeMillis());
+	}
+	
+	public static UUID randomUUID(final long timestamp)
+	{
+		final var rng = ThreadLocalRandom.current();
+		final long msb = (timestamp << 16) | 0x7000L | (rng.nextLong() & 0x0FFFL);
+		final long lsb = (rng.nextLong() & 0x3FFF_FFFF_FFFF_FFFFL) | 0x8000_0000_0000_0000L;
 		return new UUID(msb, lsb);
 	}
 }
